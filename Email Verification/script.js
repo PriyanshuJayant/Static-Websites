75022613db458bb22ecef9a9b7dd7ea98c8607a3
PriyanshuJayant <priyanshujayant8@gmail.com>
2025-07-13 16:49:27 +0530

    Static Websites

---
+++ Email Verification/script.js
@@ -0,0 +1,9 @@
+// document.addEventListener('DOMContentLoaded', function() {
+//     const slideLinks = document.querySelectorAll('.slider-nav a');
+//     let currentSlideIndex = 0;
+//     function rotateSlide() {
+//         currentSlideIndex = (currentSlideIndex + 1) % slideLinks.length;
+//         slideLinks[currentSlideIndex].click();
+//     }
+//     const slideInterval = setInterval(rotateSlide, 3000);
+// });
